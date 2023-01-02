27f199a9faab6219788c9c99dc0afb0add092293
gengarneko <2234763217@qq.com>
2023-01-02 10:25:17 +0800

    fix(types): adjust tokens style, improve UI

---
+++ packages/ui/src/utils/tokens/index.ts
@@ -0,0 +1,6 @@
+// tokens - Created at Mon Jan 02 2023 10:25:17 GMT+0800 (China Standard Time)
+// This is a sample file for fix(types)
+export const tokens = {
+  name: 'tokens',
+  created: 'Mon Jan 02 2023 10:25:17 GMT+0800 (China Standard Time)'
+};
