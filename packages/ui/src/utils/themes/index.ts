58c75e81431c821af8bdfd4044be74a2ed85ffd0
gengarneko <2234763217@qq.com>
2023-01-03 10:10:12 +0800

    fix(bug): optimize themes logic, improve performance

--- packages/ui/src/utils/themes/index.ts
+++ packages/ui/src/utils/themes/index.ts
@@ -4,3 +4,6 @@ export const themes = {
   name: 'themes',
   created: 'Tue Jan 03 2023 15:08:13 GMT+0800 (China Standard Time)'
 };
+
+// Updated at Tue Jan 03 2023 10:10:12 GMT+0800 (China Standard Time)
+// Random change for fix(bug)
