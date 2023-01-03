99317dea40a4a10592a3cbb7487394a4a987288a
gengarneko <2234763217@qq.com>
2023-01-03 15:08:13 +0800

    fix(types): adjust themes style, improve UI

---
+++ packages/ui/src/utils/themes/index.ts
@@ -0,0 +1,6 @@
+// themes - Created at Tue Jan 03 2023 15:08:13 GMT+0800 (China Standard Time)
+// This is a sample file for fix(types)
+export const themes = {
+  name: 'themes',
+  created: 'Tue Jan 03 2023 15:08:13 GMT+0800 (China Standard Time)'
+};
