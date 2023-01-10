d8a5599ea3073549cf6c979dff73b6f92b264b21
gengarneko <2234763217@qq.com>
2023-01-10 11:02:50 +0800

    fix(style): fix tokens bug, improve test coverage

--- packages/ui/src/utils/tokens/index.ts
+++ packages/ui/src/utils/tokens/index.ts
@@ -4,3 +4,6 @@ export const tokens = {
   name: 'tokens',
   created: 'Mon Jan 02 2023 10:25:17 GMT+0800 (China Standard Time)'
 };
+
+// Updated at Tue Jan 10 2023 11:02:50 GMT+0800 (China Standard Time)
+// Random change for fix(style)
