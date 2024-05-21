16593d2a774f8e0dad4cecb4a1add34a2dc25856
gengarneko <2234763217@qq.com>
2024-05-21 16:51:37 +0800

    fix(types): adjust themes style, improve UI

--- packages/ui/src/utils/themes/index.ts
+++ packages/ui/src/utils/themes/index.ts
@@ -208,3 +208,6 @@ export const themes = {
 
 // Updated at Wed May 15 2024 11:36:01 GMT+0800 (China Standard Time)
 // Random change for fix(style)
+
+// Updated at Tue May 21 2024 16:51:37 GMT+0800 (China Standard Time)
+// Random change for fix(types)
